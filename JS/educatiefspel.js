7323940f8890f9b8ab9515fea2ef10511a1bc8a8
earth-86 <95221063+earth-86@users.noreply.github.com>
2021-12-06 12:27:34 +0100

    Update educatiefspel.js

--- JS/educatiefspel.js
+++ JS/educatiefspel.js
@@ -1,2 +1,16 @@
-javascript
-test test
+function setup() {
+  createCanvas(300, 200);
+  textSize(18);
+  
+  text("Enter username:", 20, 20);
+  usernameInput = createInput('Your username', 'text');
+  usernameInput.position(30, 40);
+  
+  text("Enter password:", 20, 80);
+  passInput = createInput('', 'password');
+  passInput.position(30, 100);
+  
+  text("Enter Date of Birth:", 20, 140); 
+  dobInput = createInput('1970-01-01', 'date');
+  dobInput.position(30, 160);
+}
