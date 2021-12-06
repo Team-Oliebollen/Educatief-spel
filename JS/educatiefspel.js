17efd51b90de62fc1b4f0f2649a4cf8ff58eec6d
earth-86 <95221063+earth-86@users.noreply.github.com>
2021-12-06 12:56:48 +0100

    Update educatiefspel.js

--- JS/educatiefspel.js
+++ JS/educatiefspel.js
@@ -1,9 +1,12 @@
+function preload(){
+  img = loadImage('apple-tfa-2.jpg');
+}
 function setup() {
   createCanvas(300, 200);
+  image(img, 0, 0);
   textSize(18);
-  img = loadImage('')
   text("Answer:", 20, 20);
   usernameInput = createInput('Your answer', 'text');
   usernameInput.position(30, 40);
 }
-`// niet definitief.
+// niet definitief.
