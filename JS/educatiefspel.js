c44e8dfcc10d5976ba7951c58e9f1475b87ef120
earth-86 <95221063+earth-86@users.noreply.github.com>
2021-12-06 12:48:43 +0100

    Update educatiefspel.js

--- JS/educatiefspel.js
+++ JS/educatiefspel.js
@@ -1,8 +1,7 @@
 function setup() {
   createCanvas(300, 200);
   textSize(18);
-  
-  image
+  img = loadImage('')
   text("Answer:", 20, 20);
   usernameInput = createInput('Your answer', 'text');
   usernameInput.position(30, 40);
