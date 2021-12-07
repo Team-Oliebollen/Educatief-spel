f1561fcfba673fd702b572f475c0c099dae6dfc9
earth-86 <95221063+earth-86@users.noreply.github.com>
2021-12-07 10:16:24 +0100

    Update educatiefspel.js
    even gekeken of het werkt zonder plaatje

--- JS/educatiefspel.js
+++ JS/educatiefspel.js
@@ -1,14 +1,14 @@
 let img;
 
 function preload(){
-  img = loadImage('apple-tfa-2.jpg');
+//  img = loadImage('apple-tfa-2.jpg');
 }
 function setup() {
   createCanvas(300, 200);
   textSize(18);
 }
 function draw(){
-  image(img, 0, 0);
+//  image(img, 0, 0);
   text("Answer:", 20, 20);
   usernameInput = createInput('Your answer', 'text');
   usernameInput.position(30, 40);
