62aca995de8ae90830209aac532455f88860dc85
Ruben <60846327+RubyDupy@users.noreply.github.com>
2021-12-13 11:50:02 +0100

    Update Poppetje.js

--- JS/Poppetje.js
+++ JS/Poppetje.js
@@ -1,9 +1,29 @@
 var gameWidth = screen.width;
-var gameHeight = screen.height;
+var gameHeight = screen.height - 300;
 var backGroundColour = 'black';
+var xCharacter = gameWidth*0.5;
+var yCharacter = gameHeight*0.5;
 function setup() {
   createCanvas(gameWidth, gameHeight);
 }
 function draw() {
   background(backGroundColour);
+  moveCharacter();
+}
+function moveCharacter() {
+  fill('blue');
+  ellipse(xCharacter, yCharacter, 50, 50);
+  if(keyIsDown(68)) {
+    xCharacter++;
+  }
+  if(keyIsDown(65)) {
+    xCharacter--;
+  }
+  if(keyIsDown(83)) {
+    yCharacter++;
+  }
+  if(keyIsDown(87)) {
+    xCharacter--;
+  }
+  
 }
