249c253c68c0ee807b841f2d3c9275dccd01aebe
Ruben <60846327+RubyDupy@users.noreply.github.com>
2021-12-13 11:37:36 +0100

    Update Poppetje.js
    achtergrond

--- JS/Poppetje.js
+++ JS/Poppetje.js
@@ -1,1 +1,9 @@
-
+var gameWidth = screen.width;
+var gameHeight = screen.height;
+var backGroundColour = 'black';
+function setup() {
+  createCanvas(gamewidth, gameheight);
+}
+function draw() {
+  background(backGroundColour);
+}
