4ba2c4db7e6a448e8259e65328c7cf315ccef8fa
Ruben <60846327+RubyDupy@users.noreply.github.com>
2021-12-14 10:07:17 +0100

    Update Poppetje.js

--- JS/Poppetje.js
+++ JS/Poppetje.js
@@ -1,5 +1,5 @@
-var gameWidth = screen.width;
-var gameHeight = screen.height - 300;
+var gameWidth = screen.width - 500;
+var gameHeight = screen.height - 500;
 var backGroundColour = 'black';
 var xCharacter = gameWidth*0.5;
 var yCharacter = gameHeight*0.5;
