f83ae4cf04f6a6ac4c0ac8e2317341957af69756
Ruben <60846327+RubyDupy@users.noreply.github.com>
2022-01-17 12:29:07 +0100

    Update Poppetje.js

--- JS/Poppetje.js
+++ JS/Poppetje.js
@@ -12,7 +12,7 @@ var playerSprite;
 var backGround;
 var gravity = 0.5;
 var ySpeed = 0;
-var jumpForce = 50;
+var jumpForce = 25;
 function preload() {
   playerSprite = loadImage('../JS/images/MC_apple.png')
   backGround = loadImage('../JS/images/grasveld.jpg')
