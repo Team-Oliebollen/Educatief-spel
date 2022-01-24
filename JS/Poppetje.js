cb43fa88ddb310486db8a5cc1be2654400704800
Ruben <60846327+RubyDupy@users.noreply.github.com>
2022-01-24 11:45:06 +0100

    Update Poppetje.js

--- JS/Poppetje.js
+++ JS/Poppetje.js
@@ -73,7 +73,7 @@ function moveCharacter() {
   } else if(yCharacter == floorHeight) {
     ySpeed = 0;
   }
-  if(keyIsDown(87) && yCharacter == gameHeight -floorHeight - playerSize) {
+  if(/*keyIsDown(87) && */yCharacter == gameHeight - floorHeight - playerSize) {
     ySpeed = -jumpForce;
   }
   yCharacter = yCharacter + ySpeed;
