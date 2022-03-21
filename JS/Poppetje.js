430baed1ee47b6bd36a8d25408d698c692a233ca
Ruben <60846327+RubyDupy@users.noreply.github.com>
2022-03-21 12:41:21 +0100

    Update Poppetje.js

--- JS/Poppetje.js
+++ JS/Poppetje.js
@@ -148,7 +148,7 @@ function preload() {
 // this creates the measures of the game
 function setup() {
   createCanvas(gameWidth, gameHeight);
-  randomwords();
+  randomWords();
 }
 
 
@@ -307,7 +307,7 @@ function moveCharacter() {
     xCharacter = 0 - playerSize;
     if(levelComplete[level] == true) {
       level++;
-      randomwords();
+      randomWords();
     }
   }
   if(yCharacter > floorHeight && yCharacter < floorHeight + 5) {
