1715f49664f2bcbe500b6c48f46c53a141888973
Ruben <60846327+RubyDupy@users.noreply.github.com>
2022-04-12 22:25:11 +0200

    Update Poppetje.js

--- JS/Poppetje.js
+++ JS/Poppetje.js
@@ -190,6 +190,9 @@ function drawText() {
 }
 function randomWords() {
   correctAnswer = floor(random(1, 5));
+  word1 = random(words)[1];
+  word2 = random(words)[1];
+  word3 = random(words)[1];
   for(i = 0; i < 1; i--) {
     if(word1 == word[level] || word2 == word[level] || word3 == word[level] || word1 == word2 || word2 == word3 || word1 == word3) {
       word1 = random(words)[1];
